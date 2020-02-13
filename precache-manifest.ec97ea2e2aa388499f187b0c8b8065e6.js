085cd59de6b7740d913cd026037cb787cf8971a1
unknown <ilanmailbox@yahoo.com>
2020-02-13 11:26:50 +0200

    Updates

---
+++ precache-manifest.ec97ea2e2aa388499f187b0c8b8065e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9708ac8277903b517063bd31d05630d9",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "635352ac74447109210c",
+    "url": "/deploy-react/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "da493708623a0153b78d",
+    "url": "/deploy-react/static/css/main.ca318e85.chunk.css"
+  },
+  {
+    "revision": "635352ac74447109210c",
+    "url": "/deploy-react/static/js/2.b89ee12b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/deploy-react/static/js/2.b89ee12b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da493708623a0153b78d",
+    "url": "/deploy-react/static/js/main.3a5cba95.chunk.js"
+  },
+  {
+    "revision": "8827a40cef7a12147430",
+    "url": "/deploy-react/static/js/runtime-main.376f2844.js"
+  }
+]);
